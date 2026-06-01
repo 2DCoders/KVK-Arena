4a1664fe25d583572ec02bee8f46268f6587f5bb
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-06-01 19:49:59 +0530

    feat: add GymStepper component for enhanced workout guidance and user interaction

--- kvk-arena-ui/src/client/pages/home/gym/index.tsx
+++ kvk-arena-ui/src/client/pages/home/gym/index.tsx
@@ -1,12 +1,14 @@
 import GymHeader from "@/components/header/gym";
 import GymHero from "../../hero/gym";
 import Circle from "../../circle";
+import GymStepper from "../../gym-stepper";
 
 export default function GymHome() {
     return (
         <>
             <GymHeader />
             <GymHero />
+            <GymStepper />
             <Circle />
         </>
     )
