f064da34023902c91bdba30b411b78e76e73bf0d
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-06-02 12:07:05 +0530

    commit

--- kvk-arena-ui/src/client/pages/home/gym/index.tsx
+++ kvk-arena-ui/src/client/pages/home/gym/index.tsx
@@ -4,8 +4,15 @@ import Circle from "../../circle";
 import GymStepper from "../../gym-stepper";
 import Memberships from "../../memberships";
 import FAQ from "../../faq";
+import { useLayoutEffect } from "react";
 
 export default function GymHome() {
+
+    useLayoutEffect(() => {
+        document.documentElement.scrollTop = 0
+        window.scrollTo({ top: 0, behavior: "instant" })
+    }, []);
+
     return (
         <>
             <GymHeader />
