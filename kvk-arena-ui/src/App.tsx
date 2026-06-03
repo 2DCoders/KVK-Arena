faf808153982ad798c215dc50d4e11bf6acbab07
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-06-03 15:47:32 +0530

    commit

--- kvk-arena-ui/src/App.tsx
+++ kvk-arena-ui/src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./client/pages/home"
 import "leaflet/dist/leaflet.css";
 import GymHome from "./client/pages/home/gym";
+import BadmintonHome from "./client/pages/home/badminton";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/gym" element={<GymHome />} />
+      <Route path="/badminton" element={<BadmintonHome />} />
     </Routes>
   )
 }
