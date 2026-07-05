79da4e44eb9bf521271df4eac87284a3653d7066
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-07-05 21:28:48 +0530

    regg

--- kvk-arena-ui/src/App.tsx
+++ kvk-arena-ui/src/App.tsx
@@ -1,17 +1,17 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./client/pages/home"
 import "leaflet/dist/leaflet.css";
-import GymHome from "./client/pages/home/gym";
-import BadmintonHome from "./client/pages/home/badminton";
-import GamingHome from "./client/pages/home/gaming";
+// import GymHome from "./client/pages/home/gym";
+// import BadmintonHome from "./client/pages/home/badminton";
+// import GamingHome from "./client/pages/home/gaming";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/gym" element={<GymHome />} />
+      {/* <Route path="/gym" element={<GymHome />} />
       <Route path="/badminton" element={<BadmintonHome />} />
-      <Route path="/gaming" element={<GamingHome />} />
+      <Route path="/gaming" element={<GamingHome />} /> */}
     </Routes>
   )
 }
