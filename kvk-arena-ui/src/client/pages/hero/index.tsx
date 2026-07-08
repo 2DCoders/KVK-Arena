8344150cf2c76433a4f7a9bc0ba53af6c4cb2170
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-07-08 09:29:01 +0530

    ghfgh

--- kvk-arena-ui/src/client/pages/hero/index.tsx
+++ kvk-arena-ui/src/client/pages/hero/index.tsx
@@ -37,9 +37,9 @@ export default function Hero() {
                 Explore Now
               </button>
             </a>
-            <a href="#contact">
+            <a href="#add">
             <button className="inline-flex cursor-pointer items-center justify-center rounded-full border border-white/35 bg-white/10 px-5 py-2.5 text-xs font-semibold text-white shadow-[0_10px_24px_rgba(15,23,42,0.16)] backdrop-blur-sm transition duration-300 hover:-translate-y-0.5 hover:border-white/55 hover:bg-white/16 sm:px-8 sm:py-4 sm:text-sm">
-              Contact Us
+              Arena Membership
             </button>
             </a>
           </div>
