b760862634f39c69570dbfab20929a402117ff40
Dasun Shyaminda <dasunshyaminda0518@gmail.com>
2026-07-11 20:48:42 +0530

    kfjdj

--- kvk-arena-ui/src/App.tsx
+++ kvk-arena-ui/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./client/pages/home"
+import NotFound from "./client/pages/not-found"
 import "leaflet/dist/leaflet.css";
 // import GymHome from "./client/pages/home/gym";
 // import BadmintonHome from "./client/pages/home/badminton";
@@ -9,6 +10,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
       {/* <Route path="/gym" element={<GymHome />} />
       <Route path="/badminton" element={<BadmintonHome />} />
       <Route path="/gaming" element={<GamingHome />} /> */}
