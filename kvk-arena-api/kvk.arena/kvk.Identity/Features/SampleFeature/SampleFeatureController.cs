8a314b446768e82c69cddfcceb37e9447455100d
Lasath Rathnayake <lasathrathnayake@gmail.com>
2026-07-17 23:04:03 +0530

    nnn

--- kvk-arena-api/kvk.arena/kvk.Identity/Features/SampleFeature/SampleFeatureController.cs
+++ kvk-arena-api/kvk.arena/kvk.Identity/Features/SampleFeature/SampleFeatureController.cs
@@ -1,6 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace kvk.Identity.Features.SampleFeature;
 
-public class SampleFeatureController
+[ApiController]
+[Route("api/health-check")]
+public class SampleFeatureController : ControllerBase
 {
-    
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "Healthy", timestamp = DateTime.UtcNow });
+    }
 }
