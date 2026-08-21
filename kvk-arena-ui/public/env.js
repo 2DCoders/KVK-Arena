9a31368e9045913ba9c6a471efd471de52a2d30f
Dasun Shyaminda <122197633+LDTECHSL@users.noreply.github.com>
2026-08-21 21:40:52 +0530

    Update env.js

--- kvk-arena-ui/public/env.js
+++ kvk-arena-ui/public/env.js
@@ -2,7 +2,7 @@ window.env = {
   API_URL: "https://api-kvkarena.premiumasp.net/api/",
   // API_URL: "https://klc.runasp.net/api/",
   BASE_URL: "https://kvkarena.lk/",
-  OFFER_END_DATE: "2026-08-22",
+  OFFER_END_DATE: "2026-08-23",
   MEMBERSHIP_PRICE: "100,000",
   GYM_RATE: "10",
   BADMINTON_RATE: "10",
