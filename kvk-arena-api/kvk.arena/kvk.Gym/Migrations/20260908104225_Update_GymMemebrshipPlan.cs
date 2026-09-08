73b9295c819d9c6fee957429118ca0ddd2bcc168
Lasath Rathnayake <lasathrathnayake@gmail.com>
2026-09-08 16:14:12 +0530

    Kvk Gym Implementation

---
+++ kvk-arena-api/kvk.arena/kvk.Gym/Migrations/20260908104225_Update_GymMemebrshipPlan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace kvk.Gym.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_GymMemebrshipPlan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "MembershipPlanPendingId",
+                schema: "gym",
+                table: "Memberships",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MembershipPlanPendingId",
+                schema: "gym",
+                table: "Memberships");
+        }
+    }
+}
